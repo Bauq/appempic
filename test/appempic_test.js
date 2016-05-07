1e67ff5477a7ff9369b0a03b0c65db8d894a6947
System User <brian.uribe93@gmail.com>
2016-05-07 10:37:53 -0500

    integracion continua

---
+++ test/appempic_test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var appempic = require('../');
+var assert = require('should');
+
+describe('appempic', function () {
+
+  it('should be Brian', function () {
+    appempic.getName().should.equal('Brian');
+  });
+
+	it('should be Uribe', function () {
+	    appempic.getLastName().should.equal('Uribe');
+	  });
+});
